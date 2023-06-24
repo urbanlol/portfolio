a70b05889af5f656b7d0bb75996e3cb853407143
Dmytro Svyatodukh <johnnyjgurda@gmail.com>
2023-06-24 15:30:58 +0300

    added new project in portfolio - swapi

--- src/datas/data-projects.js
+++ src/datas/data-projects.js
@@ -8,6 +8,10 @@ import project02_460 from '../images/portfolio/svtdh-portfolio-current-site-460w
 import project02_720 from '../images/portfolio/svtdh-portfolio-current-site-720w.png';
 import project02_300 from '../images/portfolio/svtdh-portfolio-current-site-300w.png';
 import project02_520 from '../images/portfolio/svtdh-portfolio-current-site-520w.png';
+import project03_460 from '../images/portfolio/svtdh-portfolio-swapi-460w.png';
+import project03_720 from '../images/portfolio/svtdh-portfolio-swapi-720w.png';
+import project03_300 from '../images/portfolio/svtdh-portfolio-swapi-300w.png';
+import project03_520 from '../images/portfolio/svtdh-portfolio-swapi-520w.png';
 
 import project01_460wp from '../images/portfolio/svtdh-portfolio-calculate-price-460w.png?as=webp';
 import project01_720wp from '../images/portfolio/svtdh-portfolio-calculate-price-720w.png?as=webp';
@@ -17,6 +21,10 @@ import project02_460wp from '../images/portfolio/svtdh-portfolio-current-site-46
 import project02_720wp from '../images/portfolio/svtdh-portfolio-current-site-720w.png?as=webp';
 import project02_300wp from '../images/portfolio/svtdh-portfolio-current-site-300w.png?as=webp';
 import project02_520wp from '../images/portfolio/svtdh-portfolio-current-site-520w.png?as=webp';
+import project03_460wp from '../images/portfolio/svtdh-portfolio-swapi-460w.png?as=webp';
+import project03_720wp from '../images/portfolio/svtdh-portfolio-swapi-720w.png?as=webp';
+import project03_300wp from '../images/portfolio/svtdh-portfolio-swapi-300w.png?as=webp';
+import project03_520wp from '../images/portfolio/svtdh-portfolio-swapi-520w.png?as=webp';
 
 function Projects() {
   const { t } = useTranslation();
@@ -51,6 +59,21 @@ function Projects() {
       tools: 'html5, css3, javascript, react js',
       gitLink: 'https://github.com/urbanlol/portfolio',
       demoLink: 'https://portfolio-sigma-woad-26.vercel.app/',
+    },
+    {
+      title: t('list-projects.2.title'),
+      description: t('list-projects.2.description'),
+      image: project03_460,
+      mediumImage: project03_720,
+      smallImage: project03_520,
+      extraSmallImage: project03_300,
+      imageWp: project03_460wp,
+      mediumImageWp: project03_720wp,
+      smallImageWp: project03_520wp,
+      extraSmallImageWp: project03_300wp,
+      tools: 'javascript, react js, material UI react',
+      gitLink: 'https://github.com/urbanlol/star-wars-api',
+      demoLink: 'https://star-wars-api-psi.vercel.app/',
     },
   ];
 
